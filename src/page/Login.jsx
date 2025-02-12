e8cf2bec36b5e04108aa97244fac8b5d689efe8e
Amit biswas <amitbiswas9282@gmail.com>
2025-02-12 23:06:36 +0600

    login

---
+++ src/page/Login.jsx
@@ -0,0 +1,51 @@
+const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const value = { email, password };
+    console.log(value);
+  };
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="text-center lg:text-left w-1/2">
+          <img
+            src="https://i.ibb.co.com/fYW6mLkZ/f7cfe84c-3009-4138-a671-5d4fb26e416d.webp"
+            alt=""
+          />
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <h1 className="text-5xl font-bold">Login</h1>
+            <form onSubmit={handleLogin} className="fieldset">
+              <label className="fieldset-label">Email</label>
+              <input
+                name="email"
+                type="email"
+                className="input"
+                placeholder="Email"
+              />
+              <label className="fieldset-label">Password</label>
+              <input
+                name="password"
+                type="password"
+                className="input"
+                placeholder="Password"
+              />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button type="submit" className="btn btn-neutral mt-4">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
